a874ff80ad7916c7bacf1bf0ef697f8fc7d17db1
ido talmor <talmorid@gmail.com>
2018-11-05 12:43:33 +0200

    Initial Commit

---
+++ MovieTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieTest
+//
+//  Created by Ido Talmor on 05/11/2018.
+//  Copyright © 2018 Ido Talmor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
